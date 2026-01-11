46fd2958f2d7fd6225b83c303117c20434572ada
Yarlord13 <kirill.tarasov.bebebe@yandex.com>
2026-01-11 16:17:41 +0500

    Revert "Patch"
    This reverts commit 509dec8173fb2b4336e486ff7554817d43599d91.

--- src/scripts/components/card.js
+++ src/scripts/components/card.js
@@ -1,13 +1,27 @@
+export const likeCard = (likeButton) => {
+  likeButton.classList.toggle("card__like-button_is-active");
+};
+
+export const deleteCard = (cardElement) => {
+  cardElement.remove();
+};
+
+const getTemplate = () => {
+  return document
+    .getElementById("card-template")
+    .content.querySelector(".card")
+    .cloneNode(true);
+};
+
 export const createCardElement = (
   data,
-  { onPreviewPicture, onLikeIcon, onDeleteCard, onInfoButton }, // Добавлен onInfoButton
+  { onPreviewPicture, onLikeIcon, onDeleteCard },
   userId,
   cardId
 ) => {
   const cardElement = getTemplate();
   const likeButton = cardElement.querySelector(".card__like-button");
   const deleteButton = cardElement.querySelector(".card__control-button_type_delete");
-  const infoButton = cardElement.querySelector(".card__control-button_type_info"); // Добавлено
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
   const likeCountElement = cardElement.querySelector(".card__like-count");
@@ -43,11 +57,6 @@ export const createCardElement = (
   
   if (onPreviewPicture) {
     cardImage.addEventListener("click", () => onPreviewPicture({ name: data.name, link: data.link }));
-  }
-
-  // Добавлено: обработчик для кнопки информации
-  if (onInfoButton) {
-    infoButton.addEventListener("click", () => onInfoButton(cardId));
   }
   
   return cardElement;
